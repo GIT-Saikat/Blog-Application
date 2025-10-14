849f7789b274b84ebd8c83b1a678f2eb009b5374
GIT-Saikat <saikatmondal2105@gmail.com>
2025-10-14 12:27:52 +0530

    feat:defined Schema"

---
+++ prisma/migrations/20251014053937_added_user_table_and_property_name_changes/migration.sql
@@ -0,0 +1,56 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Comment` table. All the data in the column will be lost.
+  - You are about to drop the column `postId` on the `Comment` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Comment` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `author_id` to the `Comment` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `post_id` to the `Comment` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `Comment` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `author_id` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_postId_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" DROP COLUMN "createdAt",
+DROP COLUMN "postId",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "author_id" TEXT NOT NULL,
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "post_id" TEXT NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "author_id" TEXT NOT NULL,
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
